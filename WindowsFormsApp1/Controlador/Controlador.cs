4a5ce16c46b7ccfb21a6f9647449a1ce11c10f42
nachoriquelme <nachoriquelme64@gmail.com>
2024-06-14 10:30:08 -0300

    .

---
+++ WindowsFormsApp1/Controlador/Controlador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Controlador
+{
+    public class Controlador
+    {
+        public string[] Nombres = new string[1000];
+        public int Contador = 0;
+        public int[] DNIs = new int[1000];
+        public double[] Notas = new double[1000];
+
+        public void AgregarAlumno(string nombre, int dni, double nota)
+        {
+            Nombres[Contador] = nombre;
+            DNIs[Contador] = dni;
+            Notas[Contador] = nota;
+            Contador++;
+        }
+        public double CalcularPromedio()
+        {
+            double acumulador = 0;
+
+            for (int i = 0; i < Contador; i++)
+            {
+                acumulador += Notas[i];
+
+            }
+            return acumulador / Contador;
+
+        }
+        public string[] NombresProm = new string[1000];
+        public int[] DNIsProm = new int[1000];
+        public double[] NotasProm = new double[1000];
+        public int ContadorProm = 0;
+
+        public void AlumnosQueSuperanAlPromedio() 
+        {
+            
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/FormVer.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FormVer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBresultado = new System.Windows.Forms.ListBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBresultado
+            // 
+            this.LBresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBresultado.FormattingEnabled = true;
+            this.LBresultado.ItemHeight = 24;
+            this.LBresultado.Location = new System.Drawing.Point(177, 201);
+            this.LBresultado.Name = "LBresultado";
+            this.LBresultado.Size = new System.Drawing.Size(158, 100);
+            this.LBresultado.TabIndex = 0;
+            // 
+            // OK
+            // 
+            this.OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OK.Location = new System.Drawing.Point(445, 254);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(100, 47);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormVer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.LBresultado);
+            this.Name = "FormVer";
+            this.Text = "FormVer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OK;
+        public System.Windows.Forms.ListBox LBresultado;
+    }
+}
